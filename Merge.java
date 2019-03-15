c8ade73be7adb84dd118bf832ba8abe2deff5f96
Derek <dyin00@stuy.edu>
2019-03-15 11:47:28 -0400

    started

--- Merge.java
+++ Merge.java
@@ -1,3 +1,16 @@
 public class Merge{
-  
+  public static void mergesort(int[] data){
+     mergesort(data, 0, data.length-1);
+  }
+  private static void mergesort(int[] data, int lo, int hi){
+    if (lo >= hi) return;
+    for (int i =0; i < data.length; i++){
+
+    }
+
+  }
+  private static void merge(int[] data, int[] data1){
+    int[] merged = new int[data.length + data1.length];
+    
+  }
 }
