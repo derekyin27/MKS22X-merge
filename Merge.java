c770c8384f1b1fca27d46e3fb78c2d467ab33f84
Derek <dyin00@stuy.edu>
2019-03-19 11:44:53 -0400

    merge

--- Merge.java
+++ Merge.java
@@ -4,38 +4,53 @@ public class Merge{
   }
   private static void mergesort(int[] data, int lo, int hi){
     if (lo >= hi) return;
-    for (int i =lo; i < hi; i++){
-
+    int mid = (hi - lo)/2 + 1;
+    mergesort(data, lo, mid);
+    mergesort(data, mid+1, hi);
+    merge(data, lo, mid, hi);
     }
 
-  }
-  private static void merge(int[] data, int[] data1){
-    int lowest = Math.min(data.length, data1.length);
-    int[] merged = new int[data.length + data1.length];
-    int low = 0;
-    if (lowest == data.length){
-      low = data[0];
-    for (int i =0; i < data.length; i++){
-      if (data[i] < low) low = data[i];
-    }
-  }
-  if (lowest == data1.length){
-    low = data1[0];
-  for (int i =0; i < data1.length; i++){
-    if (data1[i] < low) low = data1[i];
-  }
-}
-    for (int i =0; i <lowest; i++){
 
-    for (int r = 0; r < merged.length; r++){
-      System.out.print(merged[r] + " ");
+  private static void merge(int[] data, int low, int mid, int high){
+
+    int temp1 = mid - low + 1;
+    int temp2 = high - mid;
+    int[] left = new int[temp1];
+    int[] right = new int[temp2];//making temporary arrays for merge, splits the given array bounds in half
+    for (int i = 0; i < temp1; i++){
+      left[i]=data[low+i];//copy values from original array into temps
+    }
+    for (int i = 0; i < temp2; i++){
+      right[i] = data[mid+i+1];
+    }
+    int index1 = 0;//variables for while loop
+    int index2 = 0;
+    int index3 = low;
+    while (index1 < temp1 && index2 < temp2){//easier than for loop
+      if (left[index1] <= right[index2]){//find smaller value, add it to original array starting at lowest bound index.
+        data[index3] = left[index1];
+        index1++;
+      }
+      else if (left[index1] > right[index2]){
+        data[index3] = right[index2];
+        index2++;
+      }
+      index3++;
+    }
+    // now add all remaining values
+    while (index1 < temp1){
+      data[index3] = left[index1];
+      index1++;
+      index3++;
+    }
+    while (index2 < temp2){
+      data[index3] = right[index2];
+      index2++;
+      index3++;
     }
 
     }
-  }
     public static void main(String[] args) {
       int[] test = {20, 21, 22, 23};
-      int[] test1 = {1, 7, 90, 233, 1222, 23423};
-      merge(test, test1);
     }
   }
